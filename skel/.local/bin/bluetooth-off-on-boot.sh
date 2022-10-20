32f73cc161123410999f2a48d8cf3ebf10ba56a4
IsaacJReay <isaacjacksonreay@gmail.com>
2022-10-20 20:10:09 +0700

    rename to koompi-skel

--- skel/.local/bin/bluetooth-off-on-boot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ID=$(rfkill -n -o ID,DEVICE | grep hci0 | sed "s/\ hci0//g" | sed "s/\ //")
-rfkill block $ID
--- skel/.local/bin/user-places-first-use.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -i "s/home\/dummy/home\/${USER}/g" $HOME/.local/share/user-places.xbel
-rm $HOME/.config/autostart/user-places-first-use.sh.desktop
